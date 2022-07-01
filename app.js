4ee2ddbb915c92b52cd24704e1d0aad3e420ee05
Guillem Alerany Perez <guillemfata@gmail.com>
2022-07-01 18:24:56 +0200

    canvis

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.locals.appTitle = `${capitalized(projectName)} created with IronLauncher`;
 const index = require("./routes/index.routes");
 app.use("/", index);
 
+const userSignup = require("./routes/user.routes");
+app.use("/", userSignup);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
