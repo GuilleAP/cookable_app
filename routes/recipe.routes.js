b8b695932bee25f17a5beee7bf81f84c5e7468fe
aleixmarsa <98183799+aleixmarsa@users.noreply.github.com>
2022-07-01 18:19:39 +0200

    checkbox ingredients fet

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
 
 
-
 router.post("/", (req, res, next) => {
-  console.log('REQ BODY', req.body)
   const name = req.body.name;
   console.log('HEREEEEEE!!!!!!!!!!!',name)
   res.redirect("/ingredient");
