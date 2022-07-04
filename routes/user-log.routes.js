292f725902c508de413c742df18079a08557d4ca
Guillem Alerany Perez <guillemfata@gmail.com>
2022-07-04 08:58:24 +0200

    User login i signup completats

---
+++ routes/user-log.routes.js
@@ -0,0 +1,42 @@
+const { Router } = require('express');
+const router = new Router();
+
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
+
+const User = require('../models/User.model');
+
+
+router.get('/userProfile', (req, res, next) => {
+    res.render('user_profile/profile', { userInSession: req.session.currentUser });
+})
+
+router.get('/login', (req, res, next) => {
+    res.render('user_profile/login');
+})
+
+router.post('/login', (req, res, next) => {
+    const {email, password} = req.body;
+
+    if(email === '' || password === '') {
+        res.rener('user_profile/login', {
+            errorMessage: 'Please enter both, email and password to login.'
+        });
+        return;
+    }
+
+    User.findOne({email})
+        .then((user) => {
+            if(!email) {
+                res.render('user_profile/login', {
+                    errorMessage: 'Email is not registered. Try with other user.'
+                })
+                return 
+            } else if (bcryptjs.compareSync(password, user.password)) {
+                req.session.currentUser = user;
+                res.redirect('userProfile');
+             }
+        })
+})
+
+module.exports = router;
