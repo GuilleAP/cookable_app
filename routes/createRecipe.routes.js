f2c688800435629bb72e9f798a7306e259b0d418
Aleix Marsà <98183799+aleixmarsa@users.noreply.github.com>
2022-07-07 20:17:59 +0200

    afegit error handling quan usuari o recepta estan a la bbdd

--- routes/createRecipe.routes.js
+++ routes/createRecipe.routes.js
@@ -2,87 +2,99 @@ const router = require("express").Router();
 const { isLoggedIn } = require("../middlewares/route-guard");
 const Recipe = require("../models/Recipe.model");
 const User = require("../models/User.model");
-const fileUploader = require('../config/cloudinary.config');
-
+const fileUploader = require("../config/cloudinary.config");
 
 router.get("/", isLoggedIn, (req, res, next) => {
-    res.render('createRecipe', {userInSession: req.session.currentUser});
+  res.render("createRecipe", { userInSession: req.session.currentUser });
 });
 
+router.post("/", fileUploader.single("recipe-image"), (req, res, next) => {
+  const ingredientsArr = req.body.ingredients.split(",");
+  const stepsArray = req.body.steps.split(",");
 
-router.post('/', fileUploader.single('recipe-image'), (req, res, next) => {
-    const ingredientsArr = req.body.ingredients.split(',');
-    const stepsArray = req.body.steps.split(',');
-
-    console.log(req);
-    let image;
-    if(req.file === undefined) {
-        image = 'images/default_image.png';
-    } else {
-        image = req.file.path;
+  console.log(req);
+  let image;
+  if (req.file === undefined) {
+    image = "images/default_image.png";
+  } else {
+    image = req.file.path;
+  }
+  console.log(req.body);
+  Recipe.findOne({ name: req.body.name }).then((recipe) => {
+    if (recipe) {
+      res.render("createRecipe", {
+        userInSession: req.session.currentUser,
+        errorMessage: "Name is already taken",
+      });
+      return;
     }
-    console.log(req.body);
-    Recipe.create( {
-        name: req.body.name,
-        ingredients: ingredientsArr,
-        url: req.body.url,
-        imageURL: image,
-        steps: stepsArray,
+    Recipe.create({
+      name: req.body.name,
+      ingredients: ingredientsArr,
+      url: req.body.url,
+      imageURL: image,
+      steps: stepsArray,
     })
-    .then((recipe) => {
+      .then((recipe) => {
         User.updateOne(
-            {_id : req.session.currentUser._id},
-            {$push: {recipes: recipe}})
+          { _id: req.session.currentUser._id },
+          { $push: { recipes: recipe } }
+        ).then(() => {
+          res.redirect("userProfile");
+        });
+      })
+      .catch((err) => next(err));
+  });
+});
+
+router.get("/delete-recipe/:id", (req, res, next) => {
+  const id = req.params.id;
+
+  User.findById(req.session.currentUser._id)
+    .then((user) => {
+      User.findOneAndUpdate(
+        { name: user.name },
+        { $pull: { recipes: { $in: [req.params.id] } } }
+      )
         .then(() => {
-            res.redirect('userProfile');
+          Recipe.findByIdAndDelete(req.params.id)
+            .then((response) => {
+              res.redirect("/userProfile");
+            })
+            .catch((err) => next(err));
         })
+        .catch((err) => next(err));
     })
     .catch((err) => next(err));
 });
 
-
-router.get('/delete-recipe/:id', (req, res, next) => {
-    const id = req.params.id;
-
-    User.findById(req.session.currentUser._id)
-    .then((user) => {
-        User.findOneAndUpdate(
-            {name: user.name},
-            {$pull: { recipes: { $in: [req.params.id] }}}
-        )
-        .then(() => {
-            Recipe.findByIdAndDelete(req.params.id)
-            .then((response) => {
-                res.redirect('/userProfile');
-            })
-            .catch((err) => next(err))
-        })
-        .catch((err) => next(err))
+router.get("/edit-recipe/:id", isLoggedIn, (req, res, next) => {
+  const id = req.params.id;
+  Recipe.findById(id)
+    .then((recipe) => {
+      console.log(recipe);
+      res.render("edit-recipe", {
+        recipe,
+        userInSession: req.session.currentUser,
+      });
     })
-    .catch((err) => next(err))
+    .catch((err) => next(err));
 });
 
-router.get('/edit-recipe/:id', isLoggedIn, (req, res, next) => {
+router.post(
+  "/edit-recipe/:id",
+  fileUploader.single("recipe-image"),
+  (req, res, next) => {
     const id = req.params.id;
-    Recipe.findById(id)
-        .then((recipe) => {
-            console.log(recipe)
-            res.render('edit-recipe', {recipe, userInSession: req.session.currentUser});
-        })
-        .catch(err => next(err));
-
-})
-
-router.post('/edit-recipe/:id', fileUploader.single('recipe-image'), (req, res, next) => {
-    const id = req.params.id;
-    req.body.ingredients = req.body.ingredients.split(',');
-    req.body.steps = req.body.steps.split(',');
+    req.body.ingredients = req.body.ingredients.split(",");
+    req.body.steps = req.body.steps.split(",");
 
     Recipe.findByIdAndUpdate(id, req.body)
-    .then((recipe) => {
-        res.redirect('/userProfile');
-    })
-    .catch(err => next(err));
-});
+      .then((recipe) => {
+        res.redirect("/userProfile");
+      })
+      .catch((err) => next(err));
+  }
+);
 
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,56 +1,77 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = new Router();
 
-const { isLoggedIn, isLoggedOut } = require('../middlewares/route-guard.js');
+const { isLoggedIn, isLoggedOut } = require("../middlewares/route-guard.js");
 
-const bcryptjs = require('bcryptjs');
+const bcryptjs = require("bcryptjs");
 const saltRounds = 10;
 
-const User = require('../models/User.model');
+const User = require("../models/User.model");
 
-router.get('/signup', (req, res, next) => {
-    res.render('user_signup/signup');
-})
+router.get("/signup", (req, res, next) => {
+  res.render("user_signup/signup");
+});
 
-router.post('/signup', (req, res, next) => {
+router.post("/signup", (req, res, next) => {
+  const { name, email, password } = req.body;
 
-    const {name, email, password} = req.body;
-
-    if(name === null || !email || !password) {
-        res.render('user_signup/signup', { errorMessage: 'All fields are mandatory. Please provide your username, email and password.' });
-        return;
-    }
-    const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-    if (!regex.test(password)) {
-    console.log("🚀 ~ file: user.routes.js ~ line 18 ~ router.post ~ email", email)
-        
-      res
-        .status(500)
-        .render('user_signup/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.', name: name});
-      return;
-    }
-
-    bcryptjs
-        .genSalt(saltRounds)
-        .then(salt => bcryptjs.hash(password, salt))
-        .then(hashedPassword => {
-        User.create({
-            name,
-            email,
-            password: hashedPassword
-        });
-        })
-        .then(() => {
-            res.redirect('/');
-        })
-        .catch((err) => next(err));
-})
-
-router.post('/logout', (req, res, next) => {
-    req.session.destroy(err => {
-      if (err) next(err);
-      res.redirect('/');
+  if (name === null || !email || !password) {
+    res.render("user_signup/signup", {
+      errorMessage:
+        "All fields are mandatory. Please provide your username, email and password.",
     });
+    return;
+  }
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res.status(500).render("user_signup/signup", {
+      errorMessage:
+        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
+      name: name,
+    });
+    return;
+  }
+  User.findOne({ name: name })
+    .then((user) => {
+      if (user) {
+        res.render("user_signup/signup", {
+          errorMessage: "Name is already taken",
+          email: email,
+        });
+        return;
+      }
+      User.findOne({ email: email }).then((user) => {
+        if (user) {
+          res.render("user_signup/signup", {
+            errorMessage: "Email is already taken",
+            email: email,
+          });
+          return;
+        }
+        bcryptjs
+          .genSalt(saltRounds)
+          .then((salt) => bcryptjs.hash(password, salt))
+          .then((hashedPassword) => {
+            User.create({
+              name,
+              email,
+              password: hashedPassword,
+            });
+          })
+          .then(() => {
+            res.redirect("/");
+          })
+          .catch((err) => next(err));
+      });
+    })
+    .catch((err) => next(err));
+});
+
+router.post("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) next(err);
+    res.redirect("/");
   });
+});
 
 module.exports = router;
